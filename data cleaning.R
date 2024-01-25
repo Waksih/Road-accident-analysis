964c7126ffbd47756cd539a61573841372acc06d
Waksih <kokonyaedwin@gmail.com>
2024-01-25 14:16:41 +0000

    working on time and dates

--- data cleaning.R
+++ data cleaning.R
@@ -1,7 +1,9 @@
+#install and load the necessary packages
 install.packages("tidyverse")
 library(tidyverse)
 library(readxl)
 
+#import the datasets and put them into an object
 kenya_accidents_database_xlsx_1 <- read_excel("kenya-accidents-database-xlsx-1.xlsx", +     sheet = "2016")
 
 
@@ -14,6 +16,7 @@ kenya_accidents_database_xlsx_1 <- read_excel("kenya-accidents-database-xlsx-1.x
 rd_17<-kenya_accidents_database_xlsx_1
 View(rd_17)
 
+#explore the data to see its structure and number of variables
 str(rd_16)
 str(rd_17)
 
@@ -21,41 +24,38 @@ glimpse(rd_16)
 glimpse(rd_17)
 names(rd_16)
 names(rd_17)
-rd_17 %>% 
-  select(...15) %>% 
-  unique()
 
-rd_16 %>% 
-  select(...13) %>% 
-  unique()
-
+#delete the unnecessary column from both datasets
 rd_16<-rd_16[,-13]
-View(rd_16)
-
 rd_17<-rd_17[,-15]
-View(rd_17)
+
+#change the classes of the columns to b suitable for analysis
+rd_16$COUNTY<-as.factor(rd_16$COUNTY)
+
+#convert the time column to POSIXct for better analysis
+time<-rd_16$`TIME 24 HOURS`
+time[time == "UNKNOWN TIME" | time == ""]<-NA #convert the unknown values into NA
+time1<-sprintf("%04d", as.numeric(time)) #process time values to ensure they have 4 decimal places
+#combine date and time
+datetime<- as.POSIXct(paste(rd_16$`Date DD/MM/YYYY`,time1), format = "%Y-%m-%d %H%M", tz = "Africa/Nairobi")
+rd_16$`TIME 24 HOURS`<-datetime
+
+#confirm the class of the date column in both datasets
 class(rd_17$`Date DD/MM/YYYY`)
 class(rd_16$`Date DD/MM/YYYY`)
 
+#check for missing values in the datasets 
 any(is.na(rd_16))
 any(is.na(rd_17))
 
+#check column by column for the missing values then replace them
 any(is.na(rd_16$`TIME 24 HOURS`))
 any(is.na(rd_16$`BASE/SUB BASE`))
 any(is.na(rd_16$COUNTY))
 any(is.na(rd_16$ROAD))
 any(is.na(rd_16$PLACE))
 
-j<-names(rd_16)
-j
-for (i in 1:length(j)) {
-  if(any(is.na(i))){
-    return(TRUE)
-  }else{
-    return(FALSE)
-  }
-}
-
+which(is.na(rd_16$`TIME 24 HOURS`))
 
 
 
